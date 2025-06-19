909fcc922408efa43ce03d21328d252de66ac3f0
unknown <shelaputranr@gmail.com>
2025-06-19 21:28:17 +0700

    Ganti Seeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -9,9 +9,9 @@ use App\Models\Item;
 class ItemSeeder extends Seeder{
     public function run(): void
 {
-    Item::create(['name' => 'Sepatu Nike', 'quantity'
-=> 2, 'notes' => 'Untuk Tidur']);
-    Item::create(['name' => 'Sepatu Converse', 
+    Item::create(['name' => 'Sepatu Compas', 'quantity'
+=> 2, 'notes' => 'Untuk olahraga']);
+    Item::create(['name' => 'Sepatu Jhonson', 
 'quantity' => 1, 'notes' => 'Warna hitam']);
     Item::create(['name' => 'Sepatu Pantofel', 
 'quantity' => 1, 'notes' => 'Dipakai kerja']);
