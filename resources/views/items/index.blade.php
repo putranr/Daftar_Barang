87db8801b06ccd1de0e07fe6f40b2c00af392dbe
unknown <shelaputranr@gmail.com>
2025-06-21 22:37:29 +0700

    Update Index model baru

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,28 +1,32 @@
 @extends('layout')
 
 @section('content')
-<h2>Daftar Belanja Sepatu</h2>
-<a href="{{ route('items.create') }}">Tambah Sepatu Baru</a>
-<table border="1">
-    <tr>
-        <th>Nama</th>
-        <th>Jumlah</th>
-        <th>Catatan</th>
-        <th>Aksi</th>
-    </tr>
-    @foreach ($items as $item)
-    <tr>
-        <td>{{ $item->name }}</td>
-        <td>{{ $item->quantity }}</td>
-        <td>{{ $item->notes }}</td>
-        <td>
-            <a href="{{ route('items.edit', $item->id) }}">Edit</a>
-            <form action="{{ route('items.destroy', $item->id) }}" method="POST" style="display:inline;">
-                @csrf @method('DELETE')
-                <button type="submit">Hapus</button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
+<a href="{{ route('items.create') }}" class="btn btn-primary mb-3">Tambah Sepatu Baru</a>
+
+<table class="table table-bordered table-striped">
+    <thead class="table-dark">
+        <tr>
+            <th>Nama</th>
+            <th>Jumlah</th>
+            <th>Catatan</th>
+            <th>Aksi</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($items as $item)
+        <tr>
+            <td>{{ $item->name }}</td>
+            <td>{{ $item->quantity }}</td>
+            <td>{{ $item->notes }}</td>
+            <td>
+                <a href="{{ route('items.edit', $item->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                <form action="{{ route('items.destroy', $item->id) }}" method="POST" class="d-inline">
+                    @csrf @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm">Hapus</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
 </table>
 @endsection
