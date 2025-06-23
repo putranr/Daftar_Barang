59d90e5ed24e82c452f60345eade39675735c2f4
unknown <shelaputranr@gmail.com>
2025-06-23 13:38:40 +0700

    update layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Daftar Belanja Sepatu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body class="bg-light">
 
